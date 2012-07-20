01f280b3ab04ed8c96ba920071c5ace22edc23cc
Ivan Bessarabov <ivan@bessarabov.ru>
2012-07-20 23:11:12 +0400

    More accurate pod and small refactoring

--- lib/SQL/Easy.pm
+++ lib/SQL/Easy.pm
@@ -9,89 +9,110 @@ SQL::Easy - extremely easy access to sql data
 
 =head1 DESCRIPTION
 
-On cpan there are a lot of ORMs. The problem is that sometimes ORM are too complex.
-You don't need ORM in a simple script with couple requests. ORM is sometimes difficult to use,
-you need to learn its syntax. From the other hand you already knows SQL language.
+On cpan there are a lot of ORMs. The problem is that sometimes ORM are too
+complex. You don't need ORM in a simple script with couple requests. ORM is
+sometimes difficult to use, you need to learn its syntax. From the other hand
+you already knows SQL language.
 
-SQL::Easy give you easy access to data stored in databases using well known SQL language.
+SQL::Easy give you easy access to data stored in databases using well known
+SQL language.
 
 =head1 SYNOPSIS
 
-Let image we have such db structure:
+Let image we have db 'blog' with one table:
 
     CREATE TABLE `posts` (
       `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-      `dt_post` datetime NOT NULL,
+      `dt` datetime NOT NULL,
       `title` VARCHAR(255) NOT NULL,
       PRIMARY KEY (`ID`)
     ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-    insert INTO `posts` (`dt_post`, `title`) values
-    ('2010-07-14 18:30:31', 'Hello, World!'),
-    ('2010-08-02 17:13:35', 'use perl or die')
+    insert INTO `posts` (`dt`, `title`) values
+      ('1', '2010-07-14 18:30:31', 'Hello, World!'),
+      ('2', '2010-08-02 17:13:35', 'use perl or die')
     ;
 
 Then we we can do some things with SQL::Easy
 
     use SQL::Easy;
-    use Data::Dumper; # this is only for example, you don't need it in real script
-    
-    # create object
+
     my $se = SQL::Easy->new( {
-            database => 'my_blog',
-            user => 'user',
-            password => 'secret',
-            host => '127.0.0.1',                    # default '127.0.0.1'
-            port => 3306,                           # default 3306
-            connection_check_threshold => 30,       # default 30
-            debug => 0,
+        database => 'blog',
+        user     => 'user',
+        password => 'secret',
+        host     => '127.0.0.1',           # default '127.0.0.1'
+        port     => 3306,                  # default 3306
+        connection_check_threshold => 30,  # default 30
+        debug    => 0,                     # default 0
     } );
 
-    # or, if you already have dbh:
-    # ( example how to you can use SQL::Easy with Dancer::Plugin::Database )
-    my $dbh = database();
-    my $se2 = SQL::Easy->new( { dbh=>$dbh } );
+    # get scalar
+    my $posts_count = $se->return_one("select count(id) from posts");
 
-    # let's find out how many blog posts do I have:
-    my $posts_count = $se->return_one('select count(id) from posts');
-    print Dumper $posts_count; # will print $VAR1 = 42;
+    # get list
+    my ($dt, $title) = $se->return_row(
+        "select dt, title from posts where id = ?",
+        1,
+    );
 
-    # some data about post with id 1
-    my @a = $se->return_row("select dt_post, title from posts where id = ?", 1);
-    print Dumper @a;
-    # will print:
-    =head2
-        $VAR1 = '2010-07-14 18:30:31';
-        $VAR2 = 'Hello, World!';
-    =cut
+    # get arrayref
+    my $posts = $se->return_data(
+        "select dt_post, title from posts order by id"
+    );
+    # We will get
+    #    [
+    #        {
+    #            'dt_post' => '2010-07-14 18:30:31',
+    #            'title' => 'Hello, World!'
+    #        },
+    #        {
+    #            'dt_post' => '2010-08-02 17:13:35',
+    #            'title' => 'use perl or die'
+    #        }
+    #    ];
 
-    # some data about 2 posts:
-    print Dumper $se->return_data("select dt_post, title from posts order by id limit 2");
-    =head2
-        $VAR1 = [
-                  {
-                    'dt_post' => '2010-07-14 18:30:31',
-                    'title' => 'Hello, World!'
-                  },
-                  {
-                    'dt_post' => '2010-08-02 17:13:35',
-                    'title' => 'use perl or die'
-                  }
-                ];
-    =cut
+    my $post_id = $se->insert(
+        "insert into images ( dt_post, title ) values ( now(), ? )",
+        "My new idea"
+    );
+    # $post_id is the id of the new row in table
 
-    # Next. Let add new post:
-    print Dumper $se->insert("insert into images ( dt_post, title ) values ( now(), ? )", "My new idea");
-    # It will print
-    # $VAR1 = 43;
-    # and 43 is the id of the new row in table
- 
-    # Sometimes you don't need the any return value (when you delete or update rows),
-    # you only need to execute some sql. You can do it by
-    $se->execute("update posts set title = ? where id = ?", "JFDI", 2);
+    # Sometimes you don't need the any return value (when you delete or update
+    # rows), you only need to execute some sql. You can do it by
+    $se->execute(
+        "update posts set title = ? where id = ?",
+        "JAPH",
+        2,
+    );
 
-    # If it passed more than 'connection_check_threshold' seconds between requests
-    # the module will check that db connection is alive and reconnect if it went away
+If it passed more than 'connection_check_threshold' seconds between requests
+the module will check that db connection is alive and reconnect if it went
+away.
+
+=head1 AUTHOR
+
+Ivan Bessarabov, C<< <ivan@bessarabov.ru> >>
+
+=head1 SOURCE CODE
+
+The source code for this module is hosted on GitHub
+L<https://github.com/bessarabov/SQL-Easy>
+
+=head1 BUGS
+
+Please report any bugs or feature requests in GitHub Issues
+L<https://github.com/bessarabov/SQL-Easy>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Ivan Bessarabov.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
 
 =cut
 
@@ -102,39 +123,60 @@ our $VERSION = 0.04;
 
 use DBI;
 
-=head1 GENERAL FUNCTIONS
+=head1 METHODS
+
 =cut
 
 =head2 new
- 
- * Get: 1) hash with connection information 
- * Return: 1) object 
 
-Sub creates an object
+B<Get:> 1) $class 2) $params - hashref with connection information
+
+B<Return:> 1) object
+
+    my $se = SQL::Easy->new( {
+        database => 'blog',
+        user     => 'user',
+        password => 'secret',
+        host     => '127.0.0.1',           # default '127.0.0.1'
+        port     => 3306,                  # default 3306
+        connection_check_threshold => 30,  # default 30
+        debug    => 0,                     # default 0
+    } );
+
+Or, if you already have dbh:
+
+    my $se2 = SQL::Easy->new( {
+        dbh => $dbh,
+    } );
+
+For example, if you are woring with Dancer::Plugin::Database you can use this
+command to create SQL::Easy object:
+
+    my $se3 = SQL::Easy->new( {
+        dbh => database(),
+    } );
 
 =cut
 
 sub new {
-    my $class = shift;
+    my ($class, $params) = @_;
     my $self  = {};
-    my ($params) = @_;
-    
+
     $self->{dbh} = $params->{dbh};
-    $self->{debug} = $params->{debug};
-    $self->{count} = 0;
     $self->{connection_check_threshold} = $params->{connection_check_threshold} || 30;
+    $self->{debug} = $params->{debug} || 0;
+    $self->{count} = 0;
 
     unless ($self->{dbh}) {
-        my $settings = {
-            host       => $params->{host} || '127.0.0.1',
-            port       => $params->{port} || 3306,
+        $self->{settings} = {
             db         => $params->{database},
             user       => $params->{user},
             password   => $params->{password},
+            host       => $params->{host} || '127.0.0.1',
+            port       => $params->{port} || 3306,
         };
 
-        $self->{settings} = $settings;
-        $self->{dbh} = _get_connection($settings);
+        $self->{dbh} = _get_connection($self->{settings});
     };
 
     $self->{last_connection_check} = time;
@@ -144,9 +186,10 @@ sub new {
 }
 
 =head2 return_dbh
- 
- * Get: - 
- * Return: 1) $ with dbi handler 
+
+B<Get:> 1) $self
+
+B<Return:> 1) $ with dbi handler
 
 =cut
 
@@ -154,80 +197,84 @@ sub return_dbh {
     my ($self) = @_;
 
     $self->_reconnect_if_needed();
-    
+
     return $self->{dbh};
 }
 
 =head2 return_one
- 
- * Get: 1) $ sql 2) @ bind variables
- * Return: 1) $ with first value of request result
+
+B<Get:> 1) $self 2) $sql 3) @bind_variables
+
+B<Return:> 1) $ with the first value of request result
 
 =cut
 
 sub return_one {
-    my ($self, $sql, @a) = @_;
+    my ($self, $sql, @bind_variables) = @_;
 
     $self->_reconnect_if_needed();
 
     my $sth = $self->{dbh}->prepare($sql);
     $self->log_debug($sql);
-    $sth->execute(@a);
+    $sth->execute(@bind_variables);
 
     my @row = $sth->fetchrow_array;
-    
+
     return $row[0];
 }
 
 =head2 return_row
- 
- * Get: 1) $ sql 2) @ bind variables
- * Return: 1) @ with first row in result table
+
+B<Get:> 1) $self 2) $sql 3) @bind_variables
+
+B<Return:> 1) @ with first row in result table
 
 =cut
 
 sub return_row {
-    my ($self, $sql, @a) = @_;
+    my ($self, $sql, @bind_variables) = @_;
 
     $self->_reconnect_if_needed();
 
     my $sth = $self->{dbh}->prepare($sql);
     $self->log_debug($sql);
-    $sth->execute(@a);
+    $sth->execute(@bind_variables);
 
     my @row = $sth->fetchrow_array;
-    
+
     return @row;
 }
 
 =head2 return_col
- 
- * Get: 1) $ sql 2) @ bind variables
- * Return: 1) @ with first column in result table
+
+B<Get:> 1) $self 2) $sql 3) @bind_variables
+
+B<Return:> 1) @ with first column in result table
 
 =cut
 
 sub return_col {
-    my ($self, $sql, @a) = @_;
+    my ($self, $sql, @bind_variables) = @_;
     my @return;
 
     $self->_reconnect_if_needed();
 
     my $sth = $self->{dbh}->prepare($sql);
     $self->log_debug($sql);
-    $sth->execute(@a);
+    $sth->execute(@bind_variables);
 
     while (my @row = $sth->fetchrow_array) {
-        push @return, $row[0]; 
+        push @return, $row[0];
     }
 
     return @return;
 }
 
 =head2 return_data
- 
- * Get: 1) $ sql 2) @ bind variables
- * Return: 1) $ with array of hashes with the result of the query
+
+B<Get:> 1) $self 2) $sql 3) @bind_variables
+
+B<Return:> 1) $ with array of hashes with the result of the query
 
 Sample usage:
 
@@ -243,15 +290,14 @@ Sample usage:
 =cut
 
 sub return_data {
-    my $self = shift;
-    my ($sql, @a) = @_;
+    my ($self, $sql, @bind_variables) = @_;
     my @return;
 
     $self->_reconnect_if_needed();
 
     my $sth = $self->{dbh}->prepare($sql);
     $self->log_debug($sql);
-    $sth->execute(@a);
+    $sth->execute(@bind_variables);
 
     my @cols = @{$sth->{NAME}};
 
@@ -272,13 +318,16 @@ sub return_data {
 }
 
 =head2 return_tsv_data
- 
- * Get: 1) $ sql 2) @ bind variables
- * Return: 1) $ with tab separated db data
+
+B<Get:> 1) $self 2) $sql 3) @bind_variables
+
+B<Return:> 1) $ with tab separated db data
 
 Sample usage:
 
-    print $se->return_tsv_data("select dt_post, title from posts order by id limit 2");
+    print $se->return_tsv_data(
+        "select dt_post, title from posts order by id limit 2"
+    );
 
 It will output the text below (with the tabs as separators).
 
@@ -289,14 +338,14 @@ It will output the text below (with the tabs as separators).
 =cut
 
 sub return_tsv_data {
-    my ($self, $sql, @params) = @_;
+    my ($self, $sql, @bind_variables) = @_;
     my $return;
 
     $self->_reconnect_if_needed();
 
     my $sth = $self->{dbh}->prepare($sql);
     $self->log_debug($sql);
-    $sth->execute(@params);
+    $sth->execute(@bind_variables);
 
     $return .= join ("\t", @{$sth->{NAME}}) . "\n";
 
@@ -311,61 +360,61 @@ sub return_tsv_data {
 }
 
 =head2 insert
- 
- * Get: 1) $ sql 2) @ bind variables
- * Return: 1) $ with id of inserted record
+
+B<Get:> 1) $self 2) $sql 3) @bind_variables
+
+B<Return:> 1) $ with id of inserted record
 
 Sub executes sql with bind variables and returns id of inseted record
 
 =cut
 
 sub insert {
-    my $self = shift;
-    my ($sql, @a) = @_;
+    my ($self, $sql, @bind_variables) = @_;
 
     $self->_reconnect_if_needed();
-    
+
     my $sth = $self->{dbh}->prepare($sql);
     $self->log_debug($sql);
-    $sth->execute(@a);
+    $sth->execute(@bind_variables);
 
     return $sth->{mysql_insertid};
 }
 
 =head2 execute
- 
- * Get: 1) $ sql 2) @ bind variables
- * Return: -
 
-Sub just executes sql that it recieves and returns noting interesting
+B<Get:> 1) $self 2) $sql 3) @bind_variables
+
+B<Return:> -
+
+Sub just executes sql that it recieves and returns nothing interesting
 
 =cut
 
 sub execute {
-    my $self = shift;
-    my ($sql, @a) = @_;
+    my ($self, $sql, @bind_variables) = @_;
 
     $self->_reconnect_if_needed();
 
     my $sth = $self->{dbh}->prepare($sql);
     $self->log_debug($sql);
-    $sth->execute(@a);
+    $sth->execute(@bind_variables);
 
     return 1;
 }
 
-=head2 log_debug 
- 
- * Get: 1) $ with debug text 
- * Return: -
+=head2 log_debug
 
-If the debug is turned on sub prints sql queries that are executed
+B<Get:> 1) $self 2) $sql
+
+B<Return:> -
+
+If the debug is turned on sub wll print $sql to STDERR
 
 =cut
 
 sub log_debug {
-    my $self = shift;
-    my ($sql) = @_;
+    my ($self, $sql) = @_;
 
     if ($self->{debug}) {
         $self->{count}++;
@@ -373,10 +422,22 @@ sub log_debug {
     }
 }
 
-# Method checks if last request to db was more than $self->{connection_check_threshold} seconds ago
-# If it was, then method updates stored dbh
+=begin comment _reconnect_if_needed
+
+B<Get:> 1) $self
+
+B<Return:> -
+
+Method checks if last request to db was more than
+$self->{connection_check_threshold} seconds ago. If it was, then method
+updates stored dbh.
+
+=end comment
+
+=cut
+
 sub _reconnect_if_needed {
-    my $self = shift;
+    my ($self) = @_;
 
     if (time - $self->{last_connection_check} > $self->{connection_check_threshold}) {
         if (_check_connection($self->{dbh})) {
@@ -389,21 +450,55 @@ sub _reconnect_if_needed {
 
 }
 
-# Gets hashref with connection parameters and returns db
+=begin comment _get_connection
+
+B<Get:> 1) $self
+
+B<Return:> -
+
+Gets hashref with connection parameters and returns db
+
+=end comment
+
+=cut
+
 sub _get_connection {
-    my ($s) = @_;
-    my $dsn = "DBI:mysql:database=" . $s->{db} . ";host=" . $s->{host} . ";port=" . $s->{port};
-    my $dbh = DBI->connect($dsn, $s->{user}, $s->{password}, {
-        RaiseError => 1,
-        PrintError => 0,
-        mysql_auto_reconnect => 0,
-        mysql_enable_utf8 => 1,
-    });
+    my ($self) = @_;
+
+    my $dsn = "DBI:mysql:database=" . $self->{db}
+        . ";host=" . $self->{host}
+        . ";port=" . $self->{port};
+
+    my $dbh = DBI->connect(
+        $dsn,
+        $self->{user},
+        $self->{password},
+        {
+            RaiseError => 1,
+            PrintError => 0,
+            mysql_auto_reconnect => 0,
+            mysql_enable_utf8 => 1,
+        },
+    );
+
     return $dbh;
 }
 
-# Check the connection is alive
-# Based on sub with the same name created by David Precious in Dancer::Plugin::Database
+=begin comment _check_connection
+
+B<Get:> 1) $dbh
+
+B<Return:> -
+
+Check the connection is alive.
+
+Based on sub with the same name created by David Precious in
+Dancer::Plugin::Database.
+
+=end comment
+
+=cut
+
 sub _check_connection {
     my $dbh = shift;
     return unless $dbh;
@@ -413,27 +508,17 @@ sub _check_connection {
             return 1;
         } else {
             # It was "0 but true", meaning the default DBI ping implementation
-            # Implement our own basic check, by performing a real simple query.
+            # Implement our own basic check, by performing a real simple
+            # query.
             my $ok;
             eval {
                 $ok = $dbh->do('select 1');
-            };  
+            };
             return $ok;
-        }   
+        }
     } else {
         return;
-    }   
+    }
 }
 
-=head1 AUTHOR
-
-Ivan Bessarabov, C<< <ivan@bessarabov.ru> >>
-
-=head1 SOURCE CODE 
-
-The source code for this module is hosted on GitHub http://github.com/bessarabov/SQL-Easy
-
-=cut
-
 1;
-   
